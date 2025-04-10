26cb290a3712c9091c587f77e132a31cc222018f
sidontec <sidontec@gmail.com>
2025-04-10 09:12:14 +0300

    about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,3 +162,16 @@
   });
 
 })();
+
+function slideSlider(){
+  $("#slider-scroller").css({"left":"0%","transition":"all 0s linear"});
+  $("#slider-scroller").css({"left": String(parseInt($("#slider-scroller").css("left")) - 500) + "px","transition":"all 5s linear"});
+  setTimeout(function(){moveSliderItem()}, 2635);
+}
+
+function moveSliderItem(){
+  $("#slider-scroller div").first().detach().appendTo($("#slider-scroller"));
+  slideSlider();
+}
+
+slideSlider();
